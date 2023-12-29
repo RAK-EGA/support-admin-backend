16141364912aa00f62b2149587d3b6996d028a03
Youssef Hesham Mohamed Ibrahiem <y.hesham@egac.rak.ae>
2023-12-29 11:38:45 +0200

    test(support-back):testing postAnnouncement

--- __tests__/controllers/announcement-controller.spec.js
+++ __tests__/controllers/announcement-controller.spec.js
@@ -1,3 +1,34 @@
 const {postAnnouncement} = require('../../controllers/announcement-controller');
+const Announcement = require('../../models/Announcement');
 
-it('should send a setuts code of 400 when ')
+jest.mock('../../models/Announcement');
+
+const mockSavedAnnouncement = {
+  _id: 'uniqueId',
+  title: 'title',
+  body: 'body',
+};
+
+const mockInstance = {
+  save: jest.fn().mockResolvedValue(mockSavedAnnouncement),
+};
+
+Announcement.mockImplementation(() => mockInstance);
+
+const req = {
+  body: {
+    title: 'fake-title',
+    body: 'fake-body',
+  },
+};
+
+it('should send a status code of 201 when an announcement is posted', async () => {
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await postAnnouncement(req, res);
+  expect(mockInstance.save).toHaveBeenCalledTimes(1);
+  expect(res.status).toHaveBeenCalledWith(201);
+});
